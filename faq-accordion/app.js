58b6e50f85ca43040b18e164776d1d54ea0976a4
Tracy Njoroge <tracywangari997@gmail.com>
2025-09-17 06:32:38 +0300

    Add accordion funtionality

---
+++ faq-accordion/app.js
@@ -0,0 +1,8 @@
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion) => {
+    accordion.addEventListener("click", (e) => {
+        accordion.classList.toggle('active');
+    })
+})
+
